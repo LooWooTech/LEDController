87c42f776c5e5feda593867237843057599c422c
ZhengLiangjun <zhengliangjun@gmail.com>
2015-08-22 08:08:34 +0800

    创建项目

---
+++ LoowooTech.LEDController.Server.API/APIService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LoowooTech.LEDController.Server.API
+{
+    public class APIService : IAPIService
+    {
+        public bool ShowText(string clientId, string content)
+        {
+            Console.WriteLine(content);
+            return true;
+        }
+
+        public string DownloadConfig(string clientId)
+        {
+            return "hello world!";
+        }
+    }
+}
---
+++ LoowooTech.LEDController.Server.API/IAPIService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace LoowooTech.LEDController.Server.API
+{
+    [ServiceContract]
+    public interface IAPIService
+    {
+        [OperationContract]
+        bool ShowText(string clientId, string content);
+
+        [OperationContract]
+        string DownloadConfig(string clientId);
+    }
+}
