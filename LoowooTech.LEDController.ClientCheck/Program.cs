6268ed58315a5e9675fff66f335df131ee88e18d
ZhengLiangjun <zhengliangjun@gmail.com>
2015-11-13 16:05:55 +0800

    修改检测程序错误

--- LoowooTech.LEDController.ClientCheck/Program.cs
+++ LoowooTech.LEDController.ClientCheck/Program.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Diagnostics;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
 using System.Text;
 using System.Threading;
 
@@ -21,21 +23,56 @@ namespace LoowooTech.LEDController.ClientCheck
 
         static void Main(string[] args)
         {
+            if (args == null || args.Length != 3)
+            {
+                Console.WriteLine("参数不正确");
+                Console.Read();
+                return;
+            }
+            Console.WriteLine(string.Join("\n", args));
             _clientId = args[0];
             _appName = args[1];
             _serviceUrl = args[2];
 
+            ChannelServices.RegisterChannel(new TcpClientChannel(), true);
+
             while (true)
             {
                 Thread.Sleep(1000);
-                var apps = Process.GetProcessesByName(_appName);
-                if (apps == null || apps.Length == 0)
+                var app = GetApp(_appName);
+                if (app == null)
                 {
-                    var client = GetServiceClient();
-                    client.ClearWindow(_clientId);
+                    Console.WriteLine("Doctor已被结束");
+                    try
+                    {
+                        var client = GetServiceClient();
+                        client.ShowText(_clientId, null);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("清屏失败");
+                        Console.WriteLine(ex.Message);
+                    }
                     break;
                 }
             }
+        }
+
+        private static Process GetApp(string appName)
+        {
+            var apps = Process.GetProcesses();
+            foreach (var app in apps)
+            {
+                try
+                {
+                    if (app.MainModule.ModuleName == _appName)
+                    {
+                        return app;
+                    }
+                }
+                catch { }
+            }
+            return null;
         }
     }
 }
